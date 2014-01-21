eb18c829da293bcfe587458708e6d854f2f88cbb
edsilv <e.silverton@gmail.com>
2014-01-21 16:52:07 +0000

    fix for insufficient permissions

--- loginDialogue.ts
+++ loginDialogue.ts
@@ -69,7 +69,7 @@ export class LoginDialogue extends dialogue.Dialogue {
             });
 
             if (this.inadequatePermissions) {
-                if (this.provider.pkg.assets.length > 1) {
+                if (this.provider.assetSequence.assets.length > 1) {
                     this.$nextItemButton.show();
                 }
                 this.$socialLogins.hide();
