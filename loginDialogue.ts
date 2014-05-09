8c0d962d428de39e455f4c1b7a0bf143d607d7b9
Edward Silverton <e.silverton@gmail.com>
2014-05-09 17:33:53 +0100

    sso

--- loginDialogue.ts
+++ loginDialogue.ts
@@ -7,35 +7,29 @@ import IWellcomeExtension = require("../wellcomeplayer-shared-module/iWellcomeEx
 
 export class LoginDialogue extends dialogue.Dialogue {
 
-    $message: JQuery;
-    $title: JQuery;
-    $nextItemButton: JQuery;
-    //$usernameLabel: JQuery;
-    //$username: JQuery;
-    //$passwordLabel: JQuery;
-    //$password: JQuery;
-    //$forgotButton: JQuery;
-    //$registerButton: JQuery;
-    $loginButton: JQuery;
-    $viewTermsButton: JQuery;
-    $acceptTermsButton: JQuery;
-    //$socialLogins: JQuery;
-    //$socialLoginsTitle: JQuery;
-    //$signInWithLibraryAccountButton: JQuery;
-    //$signInWithTwitterButton: JQuery;
-    //$signInWithFacebookButton: JQuery;
-    //$signInWithGoogleButton: JQuery;
-    //$signInWithOpenIDButton: JQuery;
-    $guestLogin: JQuery;
-    $libraryLogin: JQuery;
-
-    successCallback: any;
-    failureCallback: any;
-    inadequatePermissions: boolean;
-    requestedIndex: number;
     allowClose: boolean;
     allowGuestLogin: boolean;
+    allowSocialLogin: boolean;
+    failureCallback: any;
+    inadequatePermissions: boolean;
     message: string;
+    requestedIndex: number;
+    successCallback: any;
+
+    $acceptTermsButton: JQuery;
+    $facebookLoginButton: JQuery;
+    $googleLoginButton: JQuery;
+    $guestLogin: JQuery;
+    $libraryLogin: JQuery;
+    $libraryLoginButton: JQuery;
+    $message: JQuery;
+    $nextItemButton: JQuery;
+    $openIDLoginButton: JQuery;
+    $socialLogin: JQuery;
+    //$socialLoginsTitle: JQuery;
+    $title: JQuery;
+    $twitterLoginButton: JQuery;
+    $viewTermsButton: JQuery;
 
     static SHOW_LOGIN_DIALOGUE: string = 'onShowLoginDialogue';
     static HIDE_LOGIN_DIALOGUE: string = 'onHideLoginDialogue';
@@ -48,65 +42,69 @@ export class LoginDialogue extends dialogue.Dialogue {
 
     create(): void {
 
+        var that = this;
+
         this.setConfig('loginDialogue');
 
         super.create();
 
         $.subscribe(LoginDialogue.SHOW_LOGIN_DIALOGUE, (e, params) => {
-            this.open();
-            this.successCallback = params.successCallback;
-            this.failureCallback = params.failureCallback;
-            this.inadequatePermissions = params.inadequatePermissions || false;
-            this.requestedIndex = params.requestedIndex;
-            this.allowClose = params.allowClose;
-            this.allowGuestLogin = params.allowGuestLogin;
-            this.message = params.message;
+            that.open();
+            that.allowClose = params.allowClose;
+            that.allowGuestLogin = params.allowGuestLogin || false;
+            that.allowSocialLogin = params.allowSocialLogin || false;
+            that.failureCallback = params.failureCallback;
+            that.inadequatePermissions = params.inadequatePermissions || false;
+            that.message = params.message;
+            that.requestedIndex = params.requestedIndex;
+            that.successCallback = params.successCallback;
 
             // reset ui.
-            this.$message.hide();
-            //this.$guestLogin.hide();
-            this.$libraryLogin.hide();
-            this.$nextItemButton.hide();
-            //this.$socialLogins.show();
+            that.$message.hide();
+            that.$nextItemButton.hide();
+            that.$guestLogin.hide();
+            that.$libraryLogin.hide();
+            that.$socialLogin.hide();
 
-            //this.$loginButton.removeClass('disabled');
-
-            /*
-            this.$loginButton.off('click').on('click', (e) => {
-                e.preventDefault();
-
-                this.login();
-            });
-            */
-
-            if (this.inadequatePermissions) {
-                if (this.provider.sequence.assets.length > 1) {
-                    this.$nextItemButton.show();
+            if (that.inadequatePermissions) {
+                if (that.provider.getTotalCanvases() > 1) {
+                    that.$nextItemButton.show();
                 }
-                this.$libraryLogin.show();
+                that.$libraryLogin.show();
             }
 
-            if (!this.allowGuestLogin){
-                this.$guestLogin.hide();
-                this.$libraryLogin.show();
+            if (that.allowGuestLogin){
+                that.$guestLogin.show();
+            } else {
+                that.$libraryLogin.show();
             }
 
-            if (this.message) {
-                this.showMessage(this.message);
+            if (that.allowSocialLogin){
+                that.$socialLogin.show();
+            }
+
+            if (that.message) {
+                that.showMessage(that.message);
             } else {
                 // show default message.
-                this.showMessage(this.content.defaultMessage);
+                that.showMessage(that.content.defaultMessage);
             }
 
-            if (this.allowClose) {
-                this.enableClose();
+            if (that.allowClose) {
+                that.enableClose();
             } else {
-                this.disableClose();
+                that.disableClose();
             }
 
             // this has to be set here, as on click is too late to add querystring params.
-            this.$acceptTermsButton.attr('href', this.options.acceptTermsUri + '?redirectUrl=' + escape(parent.document.URL));
-            this.$loginButton.attr('href', '/handlers/auth/CasSSO.ashx?redirectUrl=' + escape(parent.document.URL));
+            var uri = escape(parent.document.URL);
+
+            that.$acceptTermsButton.attr('href', that.options.acceptTermsUri + '?redirectUrl=' + uri);
+            //that.$libraryLoginButton.attr('href', '/handlers/auth/CasSSO.ashx?redirectUrl=' + uri);
+            //that.$facebookLoginButton.attr('href', '/handlers/auth/Facebook.ashx?redirectUrl=' + uri);
+            //that.$twitterLoginButton.attr('href', '/handlers/auth/Twitter.ashx?redirectUrl=' + uri);
+            //that.$openIDLoginButton.attr('href', '/login-openid/?redirectUrl=' + uri);
+            //that.$googleLoginButton.attr('href', '/handlers/auth/Google.ashx?redirectUrl=' + uri);
         });
 
         $.subscribe(LoginDialogue.HIDE_LOGIN_DIALOGUE, (e) => {
@@ -118,36 +116,6 @@ export class LoginDialogue extends dialogue.Dialogue {
         this.$title = $('<h1>' + this.content.title + '</h1>');
         this.$content.append(this.$title);
 
-        /*
-        this.$content.append('\
-            <div class="main-login">\
-                <p class="message"></p>\
-                <a class="nextItem" href="#"></a>\
-                <div>\
-                    <label for="username"></label>\
-                    <input type="text" name="username" id="username">\
-                </div>\
-                <div>\
-                    <label for="password"></label>\
-                    <input type="password" name="password" id="password">\
-                </div>\
-                <div class="main-login-buttons">\
-                    <a class="forgot" href="#"></a>\
-                    <a class="register" href="#"></a>\
-                    <a class="button login"></a>\
-                </div>\
-            </div>\
-            <div class="social-logins">\
-                <h6></h6>\
-                <a class="social-login openid library" href="/handlers/auth/CasSSO.ashx">Library account</a>\
-                <a class="social-login twitter" href="/handlers/auth/Twitter.ashx">Twitter</a>\
-                <a class="social-login facebook" href="/handlers/auth/Facebook.ashx">Facebook</a>\
-                <a class="social-login google" href="/handlers/auth/Google.ashx">Google</a>\
-                <a class="social-login openid" href="/login-openid/">OpenID</a>\
-            </div>'
-        );
-        */
-
         this.$content.append('\
             <div class="main-login">\
                 <p class="message"></p>\
@@ -157,7 +125,13 @@ export class LoginDialogue extends dialogue.Dialogue {
                     <a class="acceptTerms button" href="#" target="_parent"></a>\
                 </div>\
                 <div class="libraryLogin">\
-                    <a class="login button" href="#" target="_parent"></a>\
+                    <a class="button" href="/handlers/auth/CasSSO.ashx"></a>\
+                </div>\
+                <div class="socialLogin">\
+                    <a class="twitter" href="/handlers/auth/Twitter.ashx">Twitter</a>\
+                    <a class="facebook" href="/handlers/auth/Facebook.ashx">Facebook</a>\
+                    <a class="google" href="/handlers/auth/Google.ashx">Google</a>\
+                    <a class="openid" href="/login-openid/">OpenID</a>\
                 </div>\
             </div>'
         );
@@ -166,64 +140,39 @@ export class LoginDialogue extends dialogue.Dialogue {
 
         this.$guestLogin = this.$content.find(".guestLogin");
         this.$libraryLogin = this.$content.find(".libraryLogin");
+        this.$socialLogin = this.$content.find(".socialLogin");
 
         this.$nextItemButton = this.$content.find(".nextItem");
         this.$nextItemButton.text(this.content.nextItem);
 
-        this.$viewTermsButton = this.$content.find(".viewTerms");
+        this.$viewTermsButton = this.$guestLogin.find(".viewTerms");
         this.$viewTermsButton.text(this.content.viewTerms);
 
-        this.$acceptTermsButton = this.$content.find(".acceptTerms");
+        this.$acceptTermsButton = this.$guestLogin.find(".acceptTerms");
         this.$acceptTermsButton.text(this.content.acceptTerms);
 
-        this.$loginButton = this.$content.find('a.login');
-        this.$loginButton.text(this.content.login);
+        this.$libraryLoginButton = this.$libraryLogin.find('.button');
+        this.$libraryLoginButton.text(this.content.login);
 
-        /*
-        this.$usernameLabel = this.$content.find("label[for='username']");
-        this.$usernameLabel.text(this.content.usernameLabel);
+        //this.$socialLoginsTitle = this.$socialLogin.find('h6');
+        //this.$socialLoginsTitle.text(this.content.socialLoginsTitle);
 
-        this.$username = this.$content.find('#username');
-        this.$username.attr('tabindex', 1);
+        this.$twitterLoginButton = this.$socialLogin.find('a.twitter');
+        this.$twitterLoginButton.text(this.content.twitter);
+        this.$twitterLoginButton.attr('tabindex', 7);
 
-        this.$passwordLabel = this.$content.find("label[for='password']");
-        this.$passwordLabel.text(this.content.passwordLabel);
+        this.$facebookLoginButton = this.$socialLogin.find('a.facebook');
+        this.$facebookLoginButton.text(this.content.facebook);
+        this.$facebookLoginButton.attr('tabindex', 8);
 
-        this.$password = this.$content.find('#password');
-        this.$password.attr('tabindex', 2);
+        this.$googleLoginButton = this.$socialLogin.find('a.google');
+        this.$googleLoginButton.text(this.content.google);
+        this.$googleLoginButton.attr('tabindex', 9);
 
-        this.$loginButton = this.$content.find('a.login');
-        this.$loginButton.text(this.content.login);
-        this.$loginButton.attr('tabindex', 3);
+        this.$openIDLoginButton = this.$socialLogin.find('a.openid');
+        this.$openIDLoginButton.text(this.content.openid);
+        this.$openIDLoginButton.attr('tabindex', 10);
 
-        this.$forgotButton = this.$content.find('a.forgot');
-        this.$forgotButton.text(this.content.forgotPassword);
-        this.$forgotButton.attr('tabindex', 4);
-
-        this.$registerButton = this.$content.find('a.register');
-        this.$registerButton.text(this.content.register);
-        this.$registerButton.attr('tabindex', 5);
-
-        this.$socialLogins = this.$content.find('.social-logins');
-
-        this.$socialLoginsTitle = this.$content.find('.social-logins h6');
-        this.$socialLoginsTitle.text(this.content.loginWith);
-
-        this.$signInWithLibraryAccountButton = this.$content.find('a.library');
-        this.$signInWithLibraryAccountButton.attr('tabindex', 6);
-
-        this.$signInWithTwitterButton = this.$content.find('a.twitter');
-        this.$signInWithTwitterButton.attr('tabindex', 7);
-
-        this.$signInWithFacebookButton = this.$content.find('a.facebook');
-        this.$signInWithFacebookButton.attr('tabindex', 8);
-
-        this.$signInWithGoogleButton = this.$content.find('a.google');
-        this.$signInWithGoogleButton.attr('tabindex', 9);
-
-        this.$signInWithOpenIDButton = this.$content.find('a.openid');
-        this.$signInWithOpenIDButton.attr('tabindex', 10);
-        */
         // initialise ui.
 
         // ui event handlers.
@@ -241,83 +190,39 @@ export class LoginDialogue extends dialogue.Dialogue {
             window.open(this.options.termsUri);
         });
 
-        /*
-        this.$forgotButton.click((e) => {
-            e.preventDefault();
-
-            this.extension.redirect(this.options.forgotPasswordUri);
-        });
-
-        this.$registerButton.click((e) => {
-            e.preventDefault();
-
-            this.extension.redirect(this.options.registerUri);
-        });
-
-        this.$signInWithLibraryAccountButton.click(function(e){
-            e.preventDefault();
-
-            var path = 'http://local.wellcomelibrary.org' + $(this).attr('href') + '?redirectUrl=' + escape(parent.document.URL);
-
-            that.extension.redirect(path);
-            window.parent.document.location.href = path;
-
-            that.extension.redirect("/handlers/auth/CasSSO.ashx?redirectUrl=http://local.wellcomelibrary.org/player/b20047459");
-        });
-
-        this.$signInWithTwitterButton.click(function(e){
+        this.$libraryLoginButton.click(function(e){
             e.preventDefault();
 
             that.extension.redirect($(this).attr('href'));
         });
 
-        this.$signInWithFacebookButton.click(function(e){
+        this.$twitterLoginButton.click(function(e){
             e.preventDefault();
 
             that.extension.redirect($(this).attr('href'));
         });
 
-        this.$signInWithGoogleButton.click(function(e){
+        this.$facebookLoginButton.click(function(e){
             e.preventDefault();
 
             that.extension.redirect($(this).attr('href'));
         });
 
-        this.$signInWithOpenIDButton.click(function(e){
+        this.$googleLoginButton.click(function(e){
             e.preventDefault();
 
             that.extension.redirect($(this).attr('href'));
         });
 
-        this.returnFunc = () => {
-            if (this.isActive){
-                this.login();
-            }
-        }
-        */
+        this.$openIDLoginButton.click(function(e){
+            e.preventDefault();
+
+            that.extension.redirect($(this).attr('href'));
+        });
+
         // hide
         this.$element.hide();
     }
-
-    /*
-    login(): void {
-
-        this.$loginButton.unbind('click');
-        this.$loginButton.addClass('disabled');
-
-        var username = this.$username.val();
-        var password = this.$password.val();
-
-        $.publish(LoginDialogue.LOGIN, [
-            {
-                username: username,
-                password: password,
-                successCallback: this.successCallback,
-                failureCallback: this.failureCallback
-            }
-        ]);
-    }
-    */
 
     resize(): void {
         super.resize();
